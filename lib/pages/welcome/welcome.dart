7f96d13c2a4ad24b6944cdb53ee3413918dbc1b4
siyuan <928082159@qq.com>
2022-01-06 20:38:34 +0800

    build the frame of the project

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('hello world'),
+        ),
+      ),
+    );
+  }
+}
